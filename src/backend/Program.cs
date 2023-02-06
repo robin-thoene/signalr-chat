9282c57b86f32be2c4314ee3f9a1d8cbc1b98dc9
Robin Thöne <>
2023-02-06 22:13:37 +0100

    add staging to appsettings

--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -1,22 +1,27 @@
 using backend.Hubs;
 
-var corsPolicyName = "ClientPolicy";
+const string CorsPolicyName = "ClientPolicy";
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json");
+var corsUrl = builder.Configuration["Cors:AllowedOrigin"];
 builder.Services.AddCors(
     opt =>
     {
         opt.AddPolicy(
-            corsPolicyName,
+            CorsPolicyName,
             policy =>
             {
-                policy.WithOrigins("http://localhost:3000")
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-                    .AllowCredentials();
+                if (corsUrl != null)
+                {
+                    policy.WithOrigins(corsUrl)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                }
             });
     });
 builder.Services.AddSignalR();
 var app = builder.Build();
-app.UseCors(corsPolicyName);
+app.UseCors(CorsPolicyName);
 app.MapHub<ChatHub>("/chatHub");
 app.Run();
